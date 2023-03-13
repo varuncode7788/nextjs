34e414c930e8bfa0b2796a3a5ce343b341804f81
varuncode7788 <varunpanchal7788@gmail.com>
2023-03-13 18:12:58 +0530

    fixed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -34,7 +34,7 @@ export async function getStaticPaths(){
     
     
     return{
-        fallback: false,
+        fallback: 'blocking',
         paths:meetups.map(meetup =>({ 
             params:{ meetupId : meetup._id.toString() }}))
         
